044ede3b7e32723cf330aaa9a367935fe53161c1
Anton Smirnov <squabbler@gmail.com>
2019-09-29 09:46:42 +0300

    HW* added create-reddit-vm.sh, bugfixes

--- packer/files/deploy.sh
+++ packer/files/deploy.sh
@@ -6,4 +6,3 @@ cd /home/appuser
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit
 bundle install
-systemctl enable puma
--- packer/files/puma-service.sh
+++ packer/files/puma-service.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
+cat > /etc/systemd/system/puma.service <<EOF
 [Unit]
 Description=Puma HTTP Server
 After=network.target
@@ -8,3 +11,8 @@ ExecStart=/usr/local/bin/puma
 Restart=always
 [Install]
 WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable puma
+systemctl start puma
